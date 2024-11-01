c447b2d82cc3934c193d4b5cc21da449e7309a4a
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-11-01 16:02:35 +0100

    Added other fields to mapper

--- hms/src/main/java/com/example/hms/room/RoomMapper.java
+++ hms/src/main/java/com/example/hms/room/RoomMapper.java
@@ -7,6 +7,14 @@ public class RoomMapper {
                 .id(room.getId())
                 .roomName(room.getRoomName())
                 .roomType(room.getRoomType())
+                .roomCapacity(room.getRoomCapacity())
+                .roomLocation(room.getRoomLocation())
+                .roomImages(room.getRoomImages())
+                .roomSize(room.getRoomSize())
+                .roomFeatures(room.getRoomFeatures())
+                .bookings(room.getBookings())
+                .roomPrice(room.getRoomPrice())
+                .roomRating(room.getRoomRating())
                 .build();
     }
     
