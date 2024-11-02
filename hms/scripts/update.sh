8d499e5fdd69a17af4b38351eca75d7278cde154
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-11-02 15:58:02 +0100

    Updated room details in the update script

--- hms/scripts/update.sh
+++ hms/scripts/update.sh
@@ -1,10 +1,10 @@
 curl -X PUT http://localhost:8080/api/v1/rooms/20/update \
 -H "Content-Type: application/json" \
 -d '{
-  "roomName": "Room 1",
+  "roomName": "Room 2",
   "roomType": "SUITE",
-  "roomCapacity": 4,
-  "roomLocation": "3rd floor",
+  "roomCapacity": 5,
+  "roomLocation": "2rd floor",
   "roomImages": ["https://picsum.photos/seed/Room1_1/800/600", "https://picsum.photos/seed/Room1_2/800/600", "https://picsum.photos/seed/Room1_3/800/600"],
   "roomSize": 10,
   "roomFeatures": ["WiFi", "TV", "Mini Fridge", "Jacuzzi"],
